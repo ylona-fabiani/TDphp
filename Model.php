4449c540d6db6b2dfb75c89aeb47f898f12c9ab6
ylow <ylona.fabiani@etu.umontpellier.fr>
2020-10-21 20:52:46 +0200

    TD2 j'en suis au 2

--- Model.php
+++ Model.php
@@ -13,15 +13,18 @@ class Model{
         $database_name = Conf::getDatabase();
         $login = Conf::getLogin();
         $password = Conf::getPassword();
-        try{
-
+        try{ //Il est important que toutes lignes de codes utilisant PDO soit dans un try - catch afin de capturer les exceptions.
+            // Connexion à la base de données
+            // Le dernier argument sert à ce que toutes les chaines de caractères
+            // en entrée et sortie de MySql soit dans le codage UTF-8
             self::$pdo = new PDO("mysql:host=$hostname;dbname=$database_name", $login, $password,
                 array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 
+            // On active le mode d'affichage des erreurs, et le lancement d'exception en cas d'erreur
             self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         } catch(PDOException $e){
-            echo $e->getMessage();
+            echo $e->getMessage(); //affiche un message d'erreur
             die();
         }
 
